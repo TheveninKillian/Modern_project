9288faa19ed28e0a67cf6cd118a78a7c825a9bda
Killian <you@example.com>
2020-05-26 16:05:30 +0200

    install rollupJS and create config

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import {
+  terser
+} from "rollup-plugin-terser";
+
+export default {
+  input: "js/index.js",
+  output: {
+    format: "esm",
+    file: "dist/index.esm.js",
+  },
+  external: ["smartcrop"],
+  plugins: [
+    commonjs(),
+    resolve(),
+    babel(),
+    terser()
+  ]
+};
